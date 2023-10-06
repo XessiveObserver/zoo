4ca826341bde0bcf06529d21fbed706aca23fed2
XessiveObserver <xessiveobserver@gmail.com>
2023-10-06 16:32:37 +0300

    added EndPoints method to main.go

--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 	"net/http"
 
 	"github.com/XessiveObserver/zoo/db"
+	"github.com/XessiveObserver/zoo/routing"
 )
 
 func main() {
 	// calls connection to the database method
 	db.InitDB()
+
+	// Calls endpoints method
+	routing.EndPoints()
 
 	fmt.Println("Server started on :8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
